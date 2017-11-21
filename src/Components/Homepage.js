daa6d03fae28502ad2a69dd6c92f0cbb44a317d5
T <622150@cognizant.com>
2017-11-21 18:47:18 +0530

    pagination started

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
-
+import { FontIcon } from 'react-md';
+import '../App.css';
 class App extends Component {
   create(){
    window.location="/Enterdetails";
@@ -10,15 +11,19 @@ class App extends Component {
       
       <div id="App">
         <fieldset id="View">
+          <FontIcon>home</FontIcon>
           <h4>Create New Measure</h4>
           <div id="Data">
             
-          <p id="Head">&#10112; Measure Details</p><br/>
+          <p id="Heads">&#10112; Measure Details</p><br/>
           <div id="Datas">
+            <i class="material-icons" id="Tag">local_offer</i>
           <input type="text" placeholder="Enter Measure Name"/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <i class="material-icons" id="Edit">rate_review</i>
           <input type="text" id="Description" placeholder="Enter Description"/>
           <br/>
           <br/><br/>
+          <i class="material-icons" id="Key">lock_open</i>
           <label id="Radio">
           <input type="radio" value="Public"/>Public
           <input type="radio" value="Private"/>Private
--- src/Components/Selectpage.js
+++ src/Components/Selectpage.js
@@ -7,7 +7,7 @@ class App extends React.Component{
     constructor() {
       super();
 		
-      this.stat = {
+      this.state = {
          data:[
             {
                Dataset: 'Dataset 1',
@@ -20,7 +20,7 @@ class App extends React.Component{
             {
                Dataset: 'Dataset 3',
                Duration: 'Jan 2014'
-            },
+            }, 
             {
                Dataset: 'Dataset 4',
                Duration: 'Jan 2014'
@@ -29,32 +29,75 @@ class App extends React.Component{
                Dataset: 'Dataset 5',
                Duration: 'Jan 2014'
             }
-         ]
+         ],
+         currentPage: 1,
+         todosPerPage: 2
       }
    }
+
+    handleClick=(event)=> {
+        this.setState({
+          currentPage: Number(event.target.id)
+        });
+      }
     render()
     {
+        const { data,currentPage, todosPerPage } = this.state;
+
+         const indexOfLastTodo = currentPage * todosPerPage;
+        const indexOfFirstTodo = indexOfLastTodo - todosPerPage;
+        const currentTodos = data.slice(indexOfFirstTodo, indexOfLastTodo);
+
+        const renderTodos = currentTodos.map((todo, index) => {
+          console.log(todo)
+          return <div id="Content" key={index}><input type="radio"/> &nbsp; &nbsp;
+         <b> {todo.Dataset} </b>
+          &nbsp; &nbsp; &nbsp; &nbsp; 
+          Last executed on - &nbsp;
+          <span id="Period"> {todo.Duration} </span>
+         </div>;
+        });
+
+        const pageNumbers = [];
+        for (let i = 1; i <= Math.ceil(data.length / todosPerPage); i++) {
+          pageNumbers.push(i);
+        }
+
+        const renderPageNumbers = pageNumbers.map(number => {
+          return (
+            <li
+              key={number}
+              id={number}
+              onClick={this.handleClick}
+            >
+              {number}
+            </li>
+          );
+        });
+
         return(
             <div>
             <fieldset id="View">
-               <FontIcon>home</FontIcon>
- 
               <p id="Title">Enter details for</p>
               <div id="EDcontent">
                 <div class="topnav">
-                 <a class="active" href="/Select Dataset">Select Dataset</a>
-                 <a href="/RefineDataset">Refine Dataset</a>
-                 <a href="/AddComputation">Add Computation</a>
-                 <a href="/OutputAttributes">Output Attributes</a>
-                 <a href="/Summary">Summary <i class="material-icons">highlight_off</i></a>                  
+                 <a class="active" href="/Select Dataset">&#10112; &nbsp;Select Dataset</a>
+                 <a href="/RefineDataset"><span class="Numberdefault">&#10113;</span>&nbsp;Refine Dataset</a>
+                 <a href="/AddComputation"><span class="Numberdefault">&#10114;</span>&nbsp;Add Computation</a>
+                 <a href="/OutputAttributes"><span class="Numberdefault">&#10115;</span>&nbsp;Output Attributes</a>
+                 <a href="/Summary"><span class="Numberdefault">&#10116;</span>&nbsp;Summary</a>                  
                </div>
-               <div id="Head">
+               <p id="Head">
                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Existing Datasets
-               </div>
+               </p>
                <fieldset id="Output">
-                   <div>
-               {this.stat.data.map((dynamicComponent, i) => <Content 
-                  key = {i} componentData = {dynamicComponent}/>)}
+                   <div >
+               {/*{this.stat.data.map((dynamicComponent, i) => <Content 
+                  key = {i} componentData = {dynamicComponent}/>)}*/}
+                   
+                
+                  {renderTodos} 
+                  {renderPageNumbers} 
             </div>
                </fieldset>
              </div>
